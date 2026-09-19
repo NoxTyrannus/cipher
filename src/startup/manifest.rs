b0c7e7fea89c9cf96183ae5f6237dfdca71f22bf
NoxTyrannus <noreply@github.com>
2026-09-20 02:53:17 +0800

    chore: bump version to 0.5.6

--- src/startup/manifest.rs
+++ src/startup/manifest.rs
@@ -5,7 +5,7 @@ use std::fs;
 use std::path::{Path, PathBuf};
 
 pub const MANIFEST_SCHEMA_VERSION: u32 = 1;
-pub const APP_VERSION: &str = "0.5.5";
+pub const APP_VERSION: &str = "0.5.6";
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PromptFileState {
